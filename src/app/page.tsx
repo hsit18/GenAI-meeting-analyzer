d5fe22de71038abbdfd8a8e02c685ac37ca324a5
Harpreet Singh <hsit18@gmail.com>
2024-03-27 00:01:34 +0530

    app cards added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,33 @@
 import Link from "next/link";
+import { Card, CardHeader, CardBody, CardFooter, SimpleGrid, Heading, Text, Button } from '@chakra-ui/react'
 
 export default function Home() {
   return (
-    <main className="flex ">
-      <Link href="/mutualfund">Mutual Fund</Link>
-      <Link href="/meeting">meeting</Link>
+    <main className="flex">
+      <SimpleGrid padding={6} spacing={4} templateColumns='repeat(auto-fill, minmax(300px, 1fr))'>
+        <Card>
+          <CardHeader>
+            <Heading size='md'>Meetings App</Heading>
+          </CardHeader>
+          <CardBody>
+            <Text>Analysis of team meeting based on AI.</Text>
+          </CardBody>
+          <CardFooter>
+            <Link href="/meeting"><Button>Start here</Button></Link>
+          </CardFooter>
+        </Card>
+        <Card>
+          <CardHeader>
+            <Heading size='md'>Mutual Fund App</Heading>
+          </CardHeader>
+          <CardBody>
+            <Text>Mutual fund list with search and details</Text>
+          </CardBody>
+          <CardFooter>
+            <Link href="/mutualfund"><Button>Start here</Button></Link>
+          </CardFooter>
+        </Card>
+      </SimpleGrid>
     </main>
   )
 }
