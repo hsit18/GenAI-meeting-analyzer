f23381c61d1e1ef7438733022851c034f7a6a1b8
Singh, Harpreet (Harpreet Singh) <hsit18@gmail.com>
2024-04-01 16:43:48 +0530

    meeting dashboard improvements

--- src/app/meeting/page.tsx
+++ src/app/meeting/page.tsx
@@ -6,36 +6,40 @@ import {
   SimpleGrid,
   Heading,
   Button,
-  Flex
+  Flex,
 } from "@chakra-ui/react";
 import Link from "next/link";
+import { MdUpload } from "react-icons/md";
 
 const MeetingPage = async () => {
   const meetings = await prisma.meeting.findMany({});
   return (
     <>
-    <Flex justifyContent={"space-between"} padding={4}>
-      <Heading size={"md"}>Meeting Dashboard </Heading> <Link href={`/meeting/upload`}><Button>Upload Meeting</Button></Link>
-    </Flex>
-    
-    <SimpleGrid
-      padding={6}
-      spacing={4}
-      templateColumns="repeat(auto-fill, minmax(400px, 1fr))"
-    >
-      {(meetings || []).map((meeting) => (
-        <Card key={meeting.id}>
-          <CardHeader>
-            <Heading size="md">{meeting.title}</Heading>
-          </CardHeader>
-          <CardFooter>
-            <Link href={`/meeting/${meeting.id}`}>
-              <Button>View Meeting Analysis</Button>
-            </Link>
-          </CardFooter>
-        </Card>
-      ))}
-    </SimpleGrid>
+      <Flex justifyContent={"space-between"} padding={4}>
+        <Heading size={"md"}>Meeting Dashboard </Heading>{" "}
+        <Link href={`/meeting/upload`}>
+          <Button leftIcon={<MdUpload />} colorScheme="teal">Upload Meeting</Button>
+        </Link>
+      </Flex>
+
+      <SimpleGrid
+        padding={6}
+        spacing={4}
+        templateColumns="repeat(auto-fill, minmax(400px, 1fr))"
+      >
+        {(meetings || []).map((meeting) => (
+          <Card key={meeting.id}>
+            <CardHeader>
+              <Heading size="md">{meeting.title}</Heading>
+            </CardHeader>
+            <CardFooter>
+              <Link href={`/meeting/${meeting.id}`}>
+                <Button>View Meeting Analysis</Button>
+              </Link>
+            </CardFooter>
+          </Card>
+        ))}
+      </SimpleGrid>
     </>
   );
 };
